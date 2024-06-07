83c1fad3ec499eab5dd2d5387b0ba9c6e5f99718
Víctor Andrés Rojas <victandres98@gmail.com>
2024-06-07 15:12:35 -0500

    chore: set up backend project with typescript and express (#2)

---
+++ backend/src/utils/helpers.ts
@@ -0,0 +1,11 @@
+const checkRequiredEnvVars = (): void => {
+  const requiredEnvVars: string[] = ["PORT"];
+
+  requiredEnvVars.forEach((envVar) => {
+    if (!process.env[envVar]) {
+      throw new Error(`Environment variable ${envVar} is missing.`);
+    }
+  });
+};
+
+export { checkRequiredEnvVars };
---
+++ backend/src/utils/index.ts
@@ -0,0 +1,1 @@
+export { checkRequiredEnvVars } from "./helpers";
