29bedcc8ca3fd2da56e09325c63edcb01585c623
Víctor Andrés Rojas <victandres98@gmail.com>
2024-07-02 18:22:06 -0500

    feat: add authentication middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,15 +2,17 @@ import 'express-async-errors';
 
 import express, { Express } from 'express';
 
-import { errorMiddleware } from './shared';
+import { currentUserMiddleware, errorMiddleware } from './shared';
 import { userRouter } from './users';
 
 const app: Express = express();
 
 app.use(express.json());
 
+app.use(currentUserMiddleware.handle.bind(currentUserMiddleware));
+
 app.use('/users', userRouter);
 
-app.use(errorMiddleware.handle);
+app.use(errorMiddleware.handle.bind(errorMiddleware));
 
 export { app };
--- backend/src/shared/infrastructure/jwt/jsonwebtoken-jwt.ts
+++ backend/src/shared/infrastructure/jwt/jsonwebtoken-jwt.ts
@@ -1,4 +1,4 @@
-import { JwtPayload, sign, verify } from 'jsonwebtoken';
+import { sign, verify } from 'jsonwebtoken';
 
 import { Jwt } from '../../domain';
 
@@ -7,8 +7,13 @@ class JsonWebTokenJwt implements Jwt {
     return sign(payload, process.env.JWT_SECRET!);
   }
 
-  verify(token: string): string | JwtPayload {
-    return verify(token, process.env.JWT_SECRET!);
+  verify<T>(token: string): T {
+    try {
+      const decoded = verify(token, process.env.JWT_SECRET!) as T;
+      return decoded;
+    } catch (error) {
+      return null as unknown as T;
+    }
   }
 }
 
