dc14f1beb30312d1f98a9f6c5d342d87a82ea6e4
Víctor Andrés Rojas <victandres98@gmail.com>
2024-07-03 00:55:41 -0500

    test: implement tests for the get event feature

--- backend/tests/events/infrastructure/event-repository/prisma-event-repository.test.ts
+++ backend/tests/events/infrastructure/event-repository/prisma-event-repository.test.ts
@@ -7,6 +7,8 @@ describe('PrismaEventRepository', () => {
 
   beforeEach(() => {
     repository = new PrismaEventRepository();
+
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
@@ -30,6 +32,33 @@ describe('PrismaEventRepository', () => {
 
       await expect(repository.create(eventData)).rejects.toThrow();
       expect(prismaMock.event.create).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('getById', () => {
+    it('should return a event when it receives an event id', async () => {
+      const eventData = generateTestData('event');
+
+      prismaMock.event.findUnique.mockResolvedValue(eventData);
+
+      await expect(repository.getById(eventData.id)).resolves.toEqual(eventData);
+      expect(prismaMock.event.findUnique).toHaveBeenCalledTimes(1);
+    });
+
+    it('should return null when the event does not exist', async () => {
+      prismaMock.event.findUnique.mockImplementation();
+
+      await expect(repository.getById(1)).resolves.toBeUndefined();
+      expect(prismaMock.event.findUnique).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw an error when the event retrieval fails', async () => {
+      const eventData = generateTestData('event');
+
+      prismaMock.event.findUnique.mockRejectedValue(new Error());
+
+      await expect(repository.getById(eventData.id)).rejects.toThrow();
+      expect(prismaMock.event.findUnique).toHaveBeenCalledTimes(1);
     });
   });
 });
--- backend/tests/events/infrastructure/rest-api/event-controller.test.ts
+++ backend/tests/events/infrastructure/rest-api/event-controller.test.ts
@@ -1,18 +1,22 @@
 import { getMockReq, getMockRes } from '@jest-mock/express';
 
-import { CreateEventUseCase, EventController } from '../../../../src/events';
+import { CreateEventUseCase, EventController, GetEventUseCase } from '../../../../src/events';
 import { StatusCode } from '../../../../src/shared';
 import { generateTestData } from '../../../utils';
 
 describe('EventController', () => {
   let controller: EventController;
   let createEventUseCase: jest.Mocked<CreateEventUseCase>;
+  let getEventUseCase: jest.Mocked<GetEventUseCase>;
 
   beforeEach(() => {
     createEventUseCase = {
       run: jest.fn(),
     } as unknown as jest.Mocked<CreateEventUseCase>;
-    controller = new EventController(createEventUseCase);
+    getEventUseCase = {
+      run: jest.fn(),
+    } as unknown as jest.Mocked<GetEventUseCase>;
+    controller = new EventController(createEventUseCase, getEventUseCase);
 
     jest.clearAllMocks();
   });
@@ -53,6 +57,53 @@ describe('EventController', () => {
       createEventUseCase.run.mockRejectedValue(new Error());
 
       await expect(controller.createEvent(req, res)).rejects.toThrow();
+      expect(res.status).not.toHaveBeenCalled();
+      expect(res.json).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('getEvent', () => {
+    it('should have the event id in the request params', async () => {
+      const id = '1';
+      const req = getMockReq({ params: { id } });
+      const { res } = getMockRes();
+
+      await controller.getEvent(req, res);
+
+      expect(req.params.id).toBeDefined();
+    });
+
+    it('should return the event data in the response when the event is found', async () => {
+      const eventData = generateTestData('event');
+      const req = getMockReq({ params: { id: eventData.id.toString() } });
+      const { res } = getMockRes();
+
+      getEventUseCase.run.mockResolvedValue(eventData);
+
+      await controller.getEvent(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(StatusCode.OK);
+      expect(res.json).toHaveBeenCalledWith(eventData);
+    });
+
+    it('should throw an error when the event id is not valid', async () => {
+      const id = 'invalid';
+      const req = getMockReq({ params: { id } });
+      const { res } = getMockRes();
+
+      await expect(controller.getEvent(req, res)).rejects.toThrow();
+      expect(res.status).not.toHaveBeenCalled();
+      expect(res.json).not.toHaveBeenCalled();
+    });
+
+    it('should throw an error when the event is not found', async () => {
+      const id = '1';
+      const req = getMockReq({ params: { id } });
+      const { res } = getMockRes();
+
+      getEventUseCase.run.mockRejectedValue(new Error());
+
+      await expect(controller.getEvent(req, res)).rejects.toThrow();
       expect(res.status).not.toHaveBeenCalled();
       expect(res.json).not.toHaveBeenCalled();
     });
