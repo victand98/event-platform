fe621c23b04a464904e3d7cf1ce345e4f8c6e420
Víctor Andrés Rojas <victandres98@gmail.com>
2024-07-03 00:56:26 -0500

    feat: implement endpoint to retrieve a single event

--- backend/src/events/infrastructure/dependencies.ts
+++ backend/src/events/infrastructure/dependencies.ts
@@ -1,11 +1,12 @@
-import { CreateEventUseCase } from '../application';
+import { CreateEventUseCase, GetEventUseCase } from '../application';
 import { PrismaEventRepository } from './event-repository';
 import { EventController } from './rest-api';
 
 const eventRepository = new PrismaEventRepository();
 
 const createEventUseCase = new CreateEventUseCase(eventRepository);
+const getEventUseCase = new GetEventUseCase(eventRepository);
 
-const eventController = new EventController(createEventUseCase);
+const eventController = new EventController(createEventUseCase, getEventUseCase);
 
 export { eventController };
