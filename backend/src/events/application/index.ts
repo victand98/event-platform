d29417228e903b08a00d8f19cc518e3e1d59588a
Víctor Andrés Rojas <victandres98@gmail.com>
2024-07-03 09:11:50 -0500

    feat: implement get events endpoint

--- backend/src/events/application/index.ts
+++ backend/src/events/application/index.ts
@@ -1,3 +1,4 @@
 export * from './create-event-usecase';
 export * from './get-event-usecase';
+export * from './get-events-usecase';
 export * from './update-event-usecase';
--- backend/src/events/infrastructure/dependencies.ts
+++ backend/src/events/infrastructure/dependencies.ts
@@ -1,13 +1,14 @@
-import { CreateEventUseCase, GetEventUseCase, UpdateEventUseCase } from '../application';
+import { CreateEventUseCase, GetEventsUseCase, GetEventUseCase, UpdateEventUseCase } from '../application';
 import { PrismaEventRepository } from './event-repository';
 import { EventController } from './rest-api';
 
 const eventRepository = new PrismaEventRepository();
 
 const createEventUseCase = new CreateEventUseCase(eventRepository);
+const getEventsUseCase = new GetEventsUseCase(eventRepository);
 const getEventUseCase = new GetEventUseCase(eventRepository);
 const updateEventUseCase = new UpdateEventUseCase(eventRepository);
 
-const eventController = new EventController(createEventUseCase, getEventUseCase, updateEventUseCase);
+const eventController = new EventController(createEventUseCase, getEventsUseCase, getEventUseCase, updateEventUseCase);
 
 export { eventController };
