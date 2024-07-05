a53b7509551513e3ab57ceb7719e2fc7cdf7916e
Víctor Andrés Rojas <victandres98@gmail.com>
2024-07-04 23:35:44 -0500

    feat: set up next-auth in the frontend

---
+++ frontend/src/auth.ts
@@ -0,0 +1,27 @@
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { signInUseCase, userRepository, UserSignInData } from './modules';
+
+export const handler = NextAuth({
+  providers: [
+    Credentials({
+      credentials: { email: {}, password: {} },
+      authorize: async (credentials) => {
+        const response = await signInUseCase(userRepository)(
+          credentials as UserSignInData
+        );
+        if ('errors' in response) {
+          throw new Error(response.errors[0].message);
+        }
+
+        return {
+          ...response,
+          id: response.id.toString(),
+          name: `${response.firstName} ${response.lastName}`,
+        };
+      },
+    }),
+  ],
+
+  pages: { signIn: '/signin', error: '/signin' },
+});
---
+++ frontend/src/types/next-auth.d.ts
@@ -0,0 +1,29 @@
+import { UserSignInResponse } from '@/modules';
+import 'next-auth';
+
+declare module 'next-auth' {
+  /**
+   * The shape of the user object returned in the OAuth providers' `profile` callback,
+   * or the second parameter of the `session` callback, when using a database.
+   */
+
+  interface User extends UserSignInResponse {}
+  /**
+   * The shape of the account object returned in the OAuth providers' `account` callback,
+   * Usually contains information about the provider being used, like OAuth tokens (`access_token`, etc).
+   */
+  interface Account {}
+
+  /**
+   * Returned by `useSession`, `auth`, contains information about the active session.
+   */
+  interface Session {}
+}
+
+// The `JWT` interface can be found in the `next-auth/jwt` submodule
+import 'next-auth/jwt';
+
+declare module 'next-auth/jwt' {
+  /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
+  interface JWT {}
+}
