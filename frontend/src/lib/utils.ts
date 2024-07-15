b7173e2422812b72f5c206e8f9315e99e798abdd
Víctor Andrés Rojas <victandres98@gmail.com>
2024-07-15 13:08:45 -0500

    feat: add get initials function

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getInitials(name: string) {
+  const names = name.split(' ');
+  return names
+    .map((n) => n.charAt(0))
+    .slice(0, 2)
+    .join('');
+}
