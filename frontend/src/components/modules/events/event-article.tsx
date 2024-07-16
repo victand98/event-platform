e0e05aff541d7f7bdbcb24fddf82dd26c85346c2
Víctor Andrés Rojas <victandres98@gmail.com>
2024-07-16 00:56:31 -0500

    feat: implement event article component

---
+++ frontend/src/components/modules/events/event-article.tsx
@@ -0,0 +1,50 @@
+import { AspectRatio, TypographyH2 } from '@/components';
+import { Event } from '@/modules';
+import { formatDate } from 'date-fns';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+export interface EventArticleProps {
+  event: Event;
+  priority?: boolean;
+}
+
+const EventArticle: React.FC<EventArticleProps> = (props) => {
+  const { event, priority } = props;
+
+  return (
+    <article key={event.id} className='group relative flex flex-col space-y-2'>
+      <AspectRatio>
+        <Image
+          src={event.image || '/assets/img/placeholder.jpg'}
+          alt={event.title}
+          className='rounded-md border bg-muted transition-colors object-cover'
+          priority={priority}
+          fill
+          sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+        />
+      </AspectRatio>
+
+      <TypographyH2 className='text-2xl font-extrabold'>
+        {event.title}
+      </TypographyH2>
+
+      <p className='text-muted-foreground line-clamp-3'>
+        {event.description} Lorem ipsum dolor sit amet, consectetur adipisicing
+        elit. Unde sint voluptatibus aut autem tenetur consequatur hic iure
+        laborum soluta quibusdam!
+      </p>
+
+      <p className='text-sm text-muted-foreground'>
+        {formatDate(event.date, 'PPp')}
+      </p>
+
+      <Link href={event.id.toString()} className='absolute inset-0'>
+        <span className='sr-only'>View Event</span>
+      </Link>
+    </article>
+  );
+};
+
+export { EventArticle };
