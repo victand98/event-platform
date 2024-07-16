6f4730ddddfafd306611d5a73af0009b2abc753f
Víctor Andrés Rojas <victandres98@gmail.com>
2024-07-15 23:07:42 -0500

    feat: create site config and main nav items

---
+++ frontend/src/config/site.ts
@@ -0,0 +1,12 @@
+const siteConfig = {
+  name: 'Event Platform',
+  description: 'A platform to manage events',
+  author: 'Víctor Andrés Rojas',
+  links: {
+    author: 'https://victorandresrojas.com',
+    github: 'https://github.com/victand98/event-platform',
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
+export { siteConfig };
