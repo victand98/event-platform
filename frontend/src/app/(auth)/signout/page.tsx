795fed659ad361dd85c0ce150cc69efe8e40efce
Víctor Andrés Rojas <victandres98@gmail.com>
2024-07-17 12:26:08 -0500

    feat: implement sign out page

---
+++ frontend/src/app/(auth)/signout/page.tsx
@@ -0,0 +1,46 @@
+import {
+  buttonVariants,
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  SignOutButton,
+} from '@/components';
+import { getServerSession } from 'next-auth';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+
+export default async function SignOutPage() {
+  const session = await getServerSession();
+
+  if (!session) {
+    redirect('/signin');
+  }
+
+  return (
+    <section className='w-full max-w-md'>
+      <Card>
+        <CardHeader className='space-y-1'>
+          <CardTitle className='text-3xl font-bold'>Sign Out</CardTitle>
+          <CardDescription>
+            Are you sure you want to sign out of your account?
+          </CardDescription>
+        </CardHeader>
+        <CardFooter className='flex flex-col gap-2 justify-between md:flex-row'>
+          <Link
+            href='/'
+            className={buttonVariants({
+              variant: 'outline',
+              className: 'w-full',
+            })}
+          >
+            No, take me back
+          </Link>
+
+          <SignOutButton />
+        </CardFooter>
+      </Card>
+    </section>
+  );
+}
