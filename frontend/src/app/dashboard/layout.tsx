2021264824ffe4e546185dcadd72eaca0711e522
Víctor Andrés Rojas <victandres98@gmail.com>
2024-07-17 00:08:24 -0500

    refactor: update dashboard layout

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import { DashboardSidebar } from '@/components';
+import { DashboardHeader, DashboardSidebar } from '@/components';
 import { sidebarNavItems } from '@/config';
 
 export default function DashboardLayout(
@@ -11,7 +11,7 @@ export default function DashboardLayout(
       <DashboardSidebar items={sidebarNavItems} />
 
       <div className='flex flex-col sm:gap-4 sm:py-4 sm:pl-14'>
-        <header className='sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6'></header>
+        <DashboardHeader />
 
         <main className='flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 p-4 sm:px-6 sm:py-0 md:gap-8'>
           {children}
