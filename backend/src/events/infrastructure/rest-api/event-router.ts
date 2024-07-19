67b75194d5350da19302461cb72d99b007c88fd4
Víctor Andrés Rojas <victandres98@gmail.com>
2024-07-18 21:06:42 -0500

    feat: protect create and update event routes

--- backend/src/events/infrastructure/rest-api/event-router.ts
+++ backend/src/events/infrastructure/rest-api/event-router.ts
@@ -1,14 +1,24 @@
 import { Router } from 'express';
 
-import { validationMiddleware } from '../../../shared';
+import { authenticationMiddleware, validationMiddleware } from '../../../shared';
 import { eventController } from '../dependencies';
 import { eventCreateValidator, eventUpdateValidator } from '../validators';
 
 const eventRouter = Router();
 
-eventRouter.post('/', validationMiddleware(eventCreateValidator), eventController.createEvent.bind(eventController));
+eventRouter.post(
+  '/',
+  authenticationMiddleware.handle,
+  validationMiddleware(eventCreateValidator),
+  eventController.createEvent.bind(eventController)
+);
 eventRouter.get('/', eventController.getEvents.bind(eventController));
 eventRouter.get('/:id', eventController.getEvent.bind(eventController));
-eventRouter.put('/:id', validationMiddleware(eventUpdateValidator), eventController.updateEvent.bind(eventController));
+eventRouter.put(
+  '/:id',
+  authenticationMiddleware.handle,
+  validationMiddleware(eventUpdateValidator),
+  eventController.updateEvent.bind(eventController)
+);
 
 export { eventRouter };
